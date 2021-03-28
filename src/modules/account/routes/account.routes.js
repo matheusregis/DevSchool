2972afd8b6af39ae8a8264ef9d949dfe31c96daf
matheusregis <matheustrai@gmail.com>
2021-03-28 01:39:07 -0300

    Structure Project

--- src/modules/account/routes/account.routes.js
+++ src/modules/account/routes/account.routes.js
@@ -4,6 +4,7 @@ const accountsRoutes = Router();
 
 accountsRoutes.post('/', async (req, res) => {
   const { name, email, password, languages, linkedin, github } = req.body;
+  res.status(200).json({ teste });
 });
 
 export default accountsRoutes;
